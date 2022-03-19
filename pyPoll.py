170b5e4486f0022531e828a6682a4f0e9be1f5e1
presj <presj98047@gmail.com>
2022-03-19 11:47:57 -0400

    removed unwanted comments

--- pyPoll.py
+++ pyPoll.py
@@ -1,10 +1,3 @@
-# The data we need to retrieve.
-# 1. Total number of votes cast
-# 2. Complete list of candidates who received votes
-# 3. Percentage of votes each candidate won
-# 4. Total number of votes each candidate won
-# 5. Winner of election based on popular vote
-
 # Add our dependencies.
 import csv
 import os
