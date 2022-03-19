a55e87fd601cbfc08f15c314484041cc8321a112
presj <presj98047@gmail.com>
2022-03-19 14:21:41 -0400

    candidates votes by percentage

--- pyPoll.py
+++ pyPoll.py
@@ -30,6 +30,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         if candidate_name not in candidate_options:
+
             # Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
 
@@ -38,6 +39,16 @@ with open(file_to_load) as election_data:
 
         #Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage}% of the vote.")       
 
 # Print the candidate vote dictionary.
 print(candidate_votes)
