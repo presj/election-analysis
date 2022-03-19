d2d361594e096ce9a8e6b67add79ba89936b37eb
presj <presj98047@gmail.com>
2022-03-19 13:22:42 -0400

    get candidate names

--- pyPoll.py
+++ pyPoll.py
@@ -6,10 +6,13 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# 1. Initialize a total vote counter.
+# Initialize a total vote counter.
 total_votes = 0
 
-# Open the election results and read the file
+# Candidate Options
+candidate_options = []
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
@@ -18,8 +21,16 @@ with open(file_to_load) as election_data:
 
     # Print each row in the CSV file.
     for row in file_reader:
-        # 2. Add to the total vote count.
+        # Add to the total vote count.
         total_votes += 1
 
-# 3. Print the total votes.
-print(total_votes)
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
